a88c3122a46a35a5894c8858191560b81deec344
richardsantos <r.santos92@live.com>
2018-09-02 18:18:53 +1000

    add app styling, add class names, fix messy code

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component {
     render() {
         return (
             <div>
-                <h1>The Weather Finder</h1>
-                <h3>Please enter your City and Country to find out the Weather conditions.</h3>
+                <h1 className="title-container__title"> The Weather Finder </h1>
+                <h3 className="title-container__subtitle"> Please enter your City and Country to find out the Weather conditions. </h3>
             </div>   
         );
     }
